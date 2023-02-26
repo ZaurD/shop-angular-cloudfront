4de56bb763280a148c8fd0c941cfdf73f9912280
Zaur Dadashov <zaur.dadashov@gmail.com>
2023-02-27 02:08:24 +0400

    change endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://trhk2fkh0l.execute-api.eu-west-1.amazonaws.com/',
+    bff: 'https://pj8ox14gw2.execute-api.eu-west-1.amazonaws.com/dev/',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
