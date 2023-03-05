d3ff1e82bf8acd9acfc328641448b949689f3278
Zaur Dadashov <zaur.dadashov@gmail.com>
2023-03-05 18:50:31 +0400

    fix product apiEndpoints and add author column in Manage-products-component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://pj8ox14gw2.execute-api.eu-west-1.amazonaws.com/dev/products',
+    product: 'https://pj8ox14gw2.execute-api.eu-west-1.amazonaws.com/dev/',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
